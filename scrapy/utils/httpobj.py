48ee3c5742a6b2d82a73fa6c795e7d8aef0ed1cc
semutter <1142528794@qq.com>
2015-08-12 16:28:26 +0800

    注释

--- scrapy/utils/httpobj.py
+++ scrapy/utils/httpobj.py
@@ -1,7 +1,17 @@
 """Helper functions for scrapy.http objects (Request, Response)"""
 
+#weakref是什么鬼？？
+#A primary use for weak references is to implement caches or mappings holding large objects,
+#where it’s desired that a large object not be kept alive solely because it appears in a cache or mapping. 
+#弱引用，当一个对象只剩弱引用时，可能会被GC回收，回收时调用创建时声明的callback函数进行相关操作
+#wekref可以做缓存用
+
 import weakref
 
+#urlparse作用是
+#它从urlstring中取得URL，并返回元组 (scheme, netloc, path, parameters, query, fragment)
+#<scheme>://<netloc>/<path>;<params>?<query>#<fragment>
+#Return a 6-tuple: (scheme, netloc, path, params, query, fragment). 
 from urlparse import urlparse
 
 _urlparse_cache = weakref.WeakKeyDictionary()
