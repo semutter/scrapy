457374a2c69ea9f5a7231804352a6ae58163c615
semutter <1142528794@qq.com>
2015-08-12 16:02:18 +0800

    注释

--- scrapy/utils/ftp.py
+++ scrapy/utils/ftp.py
@@ -6,6 +6,8 @@ def ftp_makedirs_cwd(ftp, path, first_call=True):
     argument (as a ftplib.FTP object), creating all parent directories if they
     don't exist. The ftplib.FTP object must be already connected and logged in.
     """
+    #与ftp连接相关的一些函数，但是很奇怪，与一般的文件系统上面的操作不具有一致性
+    #只有最后一次需要进入到working directory，其余的时候若存在/a/b那么可以直接创建/a/b/c
     try:
         ftp.cwd(path)
     except error_perm:
