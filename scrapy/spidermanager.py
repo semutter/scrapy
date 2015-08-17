24860d6c8753204978de91c575180eb38fd90977
semutter <1142528794@qq.com>
2015-08-17 09:46:27 +0800

    注释--not yet

--- scrapy/spidermanager.py
+++ scrapy/spidermanager.py
@@ -15,25 +15,29 @@ class SpiderManager(object):
 
     implements(ISpiderManager)
 
+    #初始化，导入给定spider_modules下面的所有spiderclass
     def __init__(self, spider_modules):
         self.spider_modules = spider_modules
         self._spiders = {}
         for name in self.spider_modules:
             for module in walk_modules(name):
                 self._load_spiders(module)
-
+    
     def _load_spiders(self, module):
+        #iter_spider_classes 返回的是spiderclass的迭代器
         for spcls in iter_spider_classes(module):
             self._spiders[spcls.name] = spcls
 
     @classmethod
     def from_settings(cls, settings):
+        #类方法，cls与self差不多，表示类本身，这里返回的是什么？？
         return cls(settings.getlist('SPIDER_MODULES'))
 
     @classmethod
     def from_crawler(cls, crawler):
         sm = cls.from_settings(crawler.settings)
         sm.crawler = crawler
+        #crawler中怎么定义的signals，以及connect
         crawler.signals.connect(sm.close_spider, signals.spider_closed)
         return sm
 
@@ -43,6 +47,7 @@ class SpiderManager(object):
         except KeyError:
             raise KeyError("Spider not found: %s" % spider_name)
         if hasattr(self, 'crawler') and hasattr(spcls, 'from_crawler'):
+            #与spcls结合来坎？？
             return spcls.from_crawler(self.crawler, **spider_kwargs)
         else:
             return spcls(**spider_kwargs)
