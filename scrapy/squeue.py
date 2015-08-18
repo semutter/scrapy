cac5749ca36775e9a80bee9a76075352b71f22a0
semutter <1142528794@qq.com>
2015-08-18 10:31:33 +0800

    注释

--- scrapy/squeue.py
+++ scrapy/squeue.py
@@ -1,7 +1,7 @@
 """
 Scheduler queues
 """
-
+#管理器队列（待处理）
 import marshal, cPickle as pickle
 
 from queuelib import queue
@@ -23,10 +23,12 @@ def _serializable_queue(queue_class, serialize, deserialize):
 
 def _pickle_serialize(obj):
     try:
+        #pickle.dumps的参数protocol协议为0：ascii，为1：二进制，为2，python2.3新支持的
         return pickle.dumps(obj, protocol=2)
     except pickle.PicklingError as e:
         raise ValueError(str(e))
 
+#构造序列化的队列类
 PickleFifoDiskQueue = _serializable_queue(queue.FifoDiskQueue, \
     _pickle_serialize, pickle.loads)
 PickleLifoDiskQueue = _serializable_queue(queue.LifoDiskQueue, \
